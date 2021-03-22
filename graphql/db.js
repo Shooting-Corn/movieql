2991c84613acb87f21ca22a99c1afe2b0b50fe03
heeseungYoo <yhs252525@gmail.com>
2021-03-23 00:20:48 +0900

    210323v heeseung movie order by id

--- graphql/db.js
+++ graphql/db.js
@@ -26,7 +26,7 @@ const connection = mysql.createPool({
 
 export const getMovies = async () => {
   const [movies] = await connection.query(
-    'SELECT * FROM `Metadata`');
+    'SELECT * FROM `Metadata` ORDER BY id');
   return movies;
 };
 
